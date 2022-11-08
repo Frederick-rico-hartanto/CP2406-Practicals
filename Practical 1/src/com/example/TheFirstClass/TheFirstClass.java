ff38471bf08e920c265b251c8b665414932743ae
Frederick-rico-hartanto <110209251+Frederick-rico-hartanto@users.noreply.github.com>
2022-11-09 00:25:29 +0800

    Add files via upload

---
+++ Practical 1/src/com/example/TheFirstClass/TheFirstClass.java
@@ -0,0 +1,8 @@
+package com.example.TheFirstClass;
+
+public class TheFirstClass {
+    public static void main(String[] args)
+    {
+        System.out.println("First Java application!");
+    }
+}

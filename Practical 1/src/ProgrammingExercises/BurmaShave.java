acc6135313bbba479e9d05f7bec6da5e2eb803e7
Fredrico <frederickrico.hartanto@my.jcu.edu.au>
2022-11-11 17:25:25 +0800

    Displayed in 4 dialog box

---
+++ Practical 1/src/ProgrammingExercises/BurmaShave.java
@@ -0,0 +1,16 @@
+package ProgrammingExercises;
+import javax.swing.JOptionPane;
+
+public class BurmaShave {
+    public static void main(String[] args){
+        String line1 = "Shaving brushes";
+        String line2 = "You' ll soon see 'em";
+        String line3 = "On a shelf";
+        String line4 = "In some museum";
+
+        JOptionPane.showMessageDialog(null, line1);
+        JOptionPane.showMessageDialog(null, line2);
+        JOptionPane.showMessageDialog(null, line3);
+        JOptionPane.showMessageDialog(null, line4);
+    }
+}

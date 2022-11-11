ccb50d7ae710a7ef30a9f414129f0c13fae1c614
Fredrico <frederickrico.hartanto@my.jcu.edu.au>
2022-11-11 17:01:23 +0800

    Print Quote

---
+++ Practical 1/src/ProgrammingExercises/MovieQuote.java
@@ -0,0 +1,9 @@
+package ProgrammingExercises;
+import javax.swing.JOptionPane;
+public class MovieQuote {
+    public static void main(String[] args){
+        String favouriteMovieQuote = "May the force be with you.";
+
+        JOptionPane.showMessageDialog(null, favouriteMovieQuote);
+    }
+}

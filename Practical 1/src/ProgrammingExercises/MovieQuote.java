173d4e163357ce52ff19fba3c06fab27da037484
Fredrico <frederickrico.hartanto@my.jcu.edu.au>
2022-11-11 17:24:04 +0800

    Edited

--- Practical 1/src/ProgrammingExercises/MovieQuote.java
+++ Practical 1/src/ProgrammingExercises/MovieQuote.java
@@ -2,8 +2,7 @@ package ProgrammingExercises;
 import javax.swing.JOptionPane;
 public class MovieQuote {
     public static void main(String[] args){
-        String favouriteMovieQuote = "May the force be with you.";
+        System.out.println("May the force be with you");
 
-        JOptionPane.showMessageDialog(null, favouriteMovieQuote);
     }
 }
--- Practical 1/src/ProgrammingExercises/MovieQuoteInfo.java
+++ Practical 1/src/ProgrammingExercises/MovieQuoteInfo.java
@@ -3,10 +3,6 @@ import javax.swing.JOptionPane;
 
 public class MovieQuoteInfo {
     public static void main(String[] args){
-        String movieQuote = "May the force be with you";
-        String quoteInfo = "Movie Title: Star wars || Character: Yoda || Year: 1977";
-
-        JOptionPane.showMessageDialog(null, movieQuote);
-        JOptionPane.showMessageDialog(null, quoteInfo);
+        System.out.println("May the force be with you" + "|| Movie Title: Star wars" + "|| Character: Yoda" + "|| Year: 1977");
     }
 }

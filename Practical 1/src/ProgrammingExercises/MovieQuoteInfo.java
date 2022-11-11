a71c3b193ec394fcb74fc89ba654b7dab49b31f5
Fredrico <frederickrico.hartanto@my.jcu.edu.au>
2022-11-11 17:01:41 +0800

    Print Quote Info

---
+++ Practical 1/src/ProgrammingExercises/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+package ProgrammingExercises;
+import javax.swing.JOptionPane;
+
+public class MovieQuoteInfo {
+    public static void main(String[] args){
+        String movieQuote = "May the force be with you";
+        String quoteInfo = "Movie Title: Star wars || Character: Yoda || Year: 1977";
+
+        JOptionPane.showMessageDialog(null, movieQuote);
+        JOptionPane.showMessageDialog(null, quoteInfo);
+    }
+}
